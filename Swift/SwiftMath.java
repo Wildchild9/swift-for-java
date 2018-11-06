7067c467105670bbff94501aadb5f51aadcb6266
Noah <noahleviwilder@gmail.com>
2018-11-06 08:35:45 -0500

    Updated copyright information

--- Swift/SwiftMath.java
+++ Swift/SwiftMath.java
@@ -6,11 +6,11 @@ package SwiftFramework.Swift;
  * ComputerScience
  *
  *
- * Last modified on 06/11/18 12:10 AM.
+ * Last modified on 06/11/18 12:46 AM.
  *
  * Copyright © 2018 Noah Wilder. All rights reserved.
  * This file is subject to the terms and conditions defined in
- * file 'LICENSE, which is part of this source code package.
+ * file 'LICENSE', which is part of this source code package.
  *
  */
 
